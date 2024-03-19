12c9189cfe46511700f144cbc8f74505d647a324
KirilZal <kirilzalov@gmail.com>
2024-03-19 19:41:12 +0100

    Add project files.

---
+++ Class Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Num1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bookText = new string[]
+            {
+            "Page 1: MARCA",
+            "Page 2: GAZETA DELLA SPORT",
+            "Page 3: GQ.",
+
+            };
+
+            FindAndReplaceManager.InitializeBook(bookText);
+
+            FindAndReplaceManager.FindNext("Page 1: MARCA");
+
+            Console.ReadLine();
+           
+
+        }
+    }
+}
---
+++ FindAndReplaceManager.cs
@@ -0,0 +1,40 @@
+﻿using Num1;
+
+namespace Num1
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public static class FindAndReplaceManager
+    {
+        private static string[] book;
+
+        public static void InitializeBook(string[] bookText)
+        {
+            book = bookText;
+        }
+
+        public static void FindNext(string str)
+        {
+            bool found = false;
+
+            for (int i = 0; i < book.Length; i++)
+            {
+                if (book[i].Contains(str))
+                {
+                    Console.WriteLine($"String '{str}' found on page {i + 1}.");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine($"String '{str}' not found in the book.");
+            }
+
+        }
+       
+    }
+
+
+}
